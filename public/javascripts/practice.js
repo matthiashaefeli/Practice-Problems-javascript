4d2107a37c1844438bfe54f106b97a4d1e7fd6f9
matthiashaefeli <haefeli.matthias@gmail.com>
2017-07-13 19:55:27 -0500

    add solution for make an array consetutive

--- public/javascripts/practice.js
+++ public/javascripts/practice.js
@@ -1,3 +1,11 @@
 function makeArrayConsecutive2(array) {
-
-}
+	var result = 0;
+	var first = array.sort()[0];
+	var last = array.sort().slice(-1);
+	for(var index = first; index < last; index++) {
+		if (array.includes(index) === false) {
+			result += 1
+		};
+	};
+	return result
+};
